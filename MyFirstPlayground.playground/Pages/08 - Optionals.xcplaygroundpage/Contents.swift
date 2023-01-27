ae6aca9c0dbc7cffb9c1c9d7728b0292b1785565
CesarArellano <raywayday@gmail.com>
2023-01-26 23:23:24 -0600

    Know how declare optionals

--- MyFirstPlayground.playground/Pages/08 - Optionals.xcplaygroundpage/Contents.swift
+++ MyFirstPlayground.playground/Pages/08 - Optionals.xcplaygroundpage/Contents.swift
@@ -1,7 +1,32 @@
-//: [Previous](@previous)
+// Optionals
 
-import Foundation
+let myString = "RWD"
+let myInt = Int(myString)
 
-var greeting = "Hello, playground"
+print(myInt)
 
-//: [Next](@next)
+if myInt != nil {
+    // Unwrap option (force unwrapping)
+    print(myInt! + 10)
+}
+
+// Optional Definition
+
+var myNewString:String?
+print(myNewString)
+
+myNewString = "Subscribed"
+if myNewString != nil {
+    // Unwrap option (force unwrapping)
+    print(myNewString)
+}
+
+// Optional Link
+
+if let myNolNilString = myNewString {
+    print(myNolNilString)
+} else {
+    print(myNewString)
+}
+
+
